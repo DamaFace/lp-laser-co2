31740b2a279bac06ad6b5e2065f69d3a5fa16fef
DamaFace <marketingdamaface@gmail.com>
2025-08-15 12:52:47 -0300

    Update PorqueEscolher.tsx

--- components/PorqueEscolher.tsx
+++ components/PorqueEscolher.tsx
@@ -58,7 +58,7 @@ const PorqueEscolher = () => {
             Pronto para sua transformação?
           </h3>
           <p className="text-base text-gray-500 mb-6 max-w-2xl mx-auto">
-            Veja como você ficaria com Ultraformer
+            Veja como você ficaria com Laser CO2
           </p>
           <Link
             href="#resultados"
